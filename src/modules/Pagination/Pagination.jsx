f80f083b4b94e2dc677961be7bc1df2b7813b28f
Anatolyjs <Akebecz@inbox.ru>
2023-05-24 18:03:04 +0300

    added adaptive

--- src/modules/Pagination/Pagination.jsx
+++ src/modules/Pagination/Pagination.jsx
@@ -1,16 +1,44 @@
 import ReactPaginate from "react-paginate";
+import { useEffect } from "react";
+import { useState } from "react";
 
 import "./Pagination.scss";
 import { BtnArrowNext, BtnArrowPrev } from "../../ui/BtnArrow/BtnsArrow";
 
+
 export const Pagination = ({ pageCount, callbackFun, initialPage }) => {
+  const [pagesRange, setPagesRange] = useState(2);
+
+  const widthChanhed = () => {
+    if (window.innerWidth > 480) {
+      setPagesRange(2)
+    } else {
+      setPagesRange(1);
+    }
+  }
+
+  useEffect(() => {
+
+    if (window.innerWidth > 480) {
+      setPagesRange(2)
+    } else {
+      setPagesRange(1);
+    }
+    
+    window.addEventListener('resize', widthChanhed);
+
+    return () => {
+      window.removeEventListener('resize', widthChanhed);
+    }
+  }, []);
+
   return (
     <>
       <ReactPaginate
         breakLabel="..."
         nextLabel={<BtnArrowNext />}
         onPageChange={callbackFun}
-        pageRangeDisplayed={2}
+        pageRangeDisplayed={pagesRange}
         pageCount={pageCount}
         forcePage={initialPage}
         previousLabel={<BtnArrowPrev />}
@@ -22,6 +50,7 @@ export const Pagination = ({ pageCount, callbackFun, initialPage }) => {
         disabledClassName="disabled-button"
         previousClassName="btn"
         nextClassName="btn"
+        marginPagesDisplayed={pagesRange}
       />
     </>
   );
